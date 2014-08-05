6796ffe15ea6d559ef7e8a2be552345a77dc97f6
aripalo <ari@aripalo.fi>
2014-08-05 20:58:59 +0300

    add comments

--- test/suites/index.js
+++ test/suites/index.js
@@ -1,2 +1,7 @@
+/*
+ * Expose all test suites as commonjs module object
+ * =============================================================================
+ * - implements https://www.npmjs.org/package/require-directory
+ */
 var requireDirectory = require('require-directory');
 module.exports = requireDirectory(module);
